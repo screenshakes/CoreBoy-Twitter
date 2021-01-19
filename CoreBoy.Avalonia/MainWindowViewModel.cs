1ca66b77e2a6f08dbcca08878adef85fe71964a2
Constantin <constantin.lietard@protonmail.com>
2021-01-18 22:16:09 -0500

    Cleaning

--- CoreBoy.Avalonia/MainWindowViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace CoreBoy.Avalonia
-{
-    public class MainWindowViewModel
-    {
-        public IReadOnlyList<MenuItemViewModel> MenuItems { get; set; }
-    }
-}
